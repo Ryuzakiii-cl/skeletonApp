ab193274875e3e7adbd21298eb1cbc34080e22bd
Ryuzakiii-cl <ch.lhoest@duocuc.cl>
2023-09-17 01:56:29 -0300

    a

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, OnInit, Renderer2 } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute } from "@angular/router";
 import { AlertController } from '@ionic/angular';
 
